723fd380daca50b8bc66899a174ba56f1235d186
Vlad Shchelkunov <vlad777.od@gmail.com>
2022-06-29 11:33:50 +0300

    123

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,10 @@
 import { galleryItems } from './gallery-items.js';
-import * as basicLightbox from 'basiclightbox'
+
 // Change code below this line
 const galleryContainer = document.querySelector('.gallery');
-const galleryBlock = document.querySelector(".gallery__item");
-const galleryImage = document.querySelector(".gallery__image")
-
-// const lightBoxModal = document.querySelector(".modal")
-// const buttonCloseOnModal = document.querySelector(".close-button")
-
 const blockMarkUp = createImagesGallery(galleryItems);
-galleryContainer.insertAdjacentHTML("afterbegin",blockMarkUp);
-galleryContainer.addEventListener ('click', onGalleryContainerClick);
+galleryContainer.insertAdjacentHTML("afterbegin", blockMarkUp);
+galleryContainer.addEventListener('click', onGalleryContainerClick);
 
 function createImagesGallery (items) {
     return items
@@ -19,56 +13,44 @@ function createImagesGallery (items) {
           <a class="gallery__link" href="${original}">
               <img class="gallery__image"
               src="${preview}"
-              data-source="large-image.jpg"
+              data-source="${original}"
               alt="${description}"
               />
           </a>
       </div>`)
       .join('');
-  };
-
-function onGalleryContainerClick (event) {
+};
+  
+function onGalleryContainerClick(event) {
     event.preventDefault();
-    console.log(event.target)
-    if (!event.target.classList.contains("gallery__image")) {
-    return;
+    if (event.target.nodeName !== 'IMG') {
+        return;
     }
-    galleryBlock.classList.add('is-open');
 
+    const instance = basicLightbox.create(
+        `<img src="${event.target.dataset.source}">`,
+        {
+            onShow: function () {
+                const newActiveBlock = event.target;
+                newActiveBlock.classList.add("is-open");
+            },
 
-}
+            onClose: function () {
+                event.preventDefault();
+                const currentActiveImage = document.querySelector(".gallery__image.is-open");
+                currentActiveImage?.classList.remove("is-open");
+            }
+        }
+    )
 
-// const instance = basicLightbox.create(`
-//     <img src="assets/images/image.png" width="800" height="600">
-// `)
-// instance.show()
-
-
-
-// function onClickHandlerClose(event) {
-//     event.preventDefault(); 
-//     lightBoxModal.classList.remove("is-open");
-// }
-
-// function closeModalByEscape(event) {
-//     if (event.code === "Escape") {
-//         onClickHandlerClose();
-//       }
-//   }
-
-
-
-
-
-
+    window.addEventListener("keydown", function (event) {
+        if (event.code === "Escape") {
+            instance.close();
+        }
+    })
+    
+    instance.show()
+    }
 
 
 
-// const currentActiveBlock = document.querySelector(".gallery__item .is-open");
-// if (currentActiveBlock) {
-//     currentActiveBlock.classList.remove(".is-open");
-// } 
-// currentActiveBlock?.classList.remove(".is-open");
-// const newActiveBlock = event.target;
-// const parentColorCard = newActiveBlock.closest(".gallery__item")
-// parentColorCard.classList.add('is-open');
